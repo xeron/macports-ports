739d1a8687493827dd459a85ef53a6824d73faee
Lawrence Velázquez <larryv@macports.org>
2013-08-08 04:42:19 +0000

    Create topic branch for adding subports to subversion.
    git-svn-id: https://svn.macports.org/repository/macports/branches/subversion-subports/dports@109117 d073be05-634f-4543-b044-5fe20cf6d1d6

--- _resources/port1.0/livecheck/fallback.tcl
+++
@@ -1,6 +0,0 @@
-# $Id$
-#
-# If the default type cannot be determined from the mirror, this file is
-# sourced.
-
-source [getdefaultportresourcepath "port1.0/livecheck"]/freecode.tcl
--- _resources/port1.0/livecheck/freshmeat.tcl
+++
@@ -1,5 +0,0 @@
-# $Id$
-#
-# Provide freshmeat as an alias for freecode
-
-source [getdefaultportresourcepath "port1.0/livecheck"]/freecode.tcl
